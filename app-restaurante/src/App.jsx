62a7e0ebfa6e6757782dd03659ade91940a64f07
HiroitoS <hitoito.sanchez@gmail.com>
2023-12-09 14:59:35 -0500

    header

--- app-restaurante/src/App.jsx
+++ app-restaurante/src/App.jsx
@@ -1,31 +1,38 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route,  NavLink } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link  } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import Gallery from './pages/Gallery'
 
 const App = () => {
   return (
     <>
-      <header className='bg-slate-300 '>
-        <div className='container-lg '>
+    <BrowserRouter>
+      <header className="bg-slate-300 py-4 px-6">
+        <div className="container mx-auto flex justify-between items-center">
           <div>
-            <img src="https://placehold.co/40x40"/>
+            <img src="https://placehold.co/150x80" />
           </div>
-          <nav>
-            <NavLink to='/'>Home</NavLink>
+
+          <nav className="flex justify-center gap-5">
+            <Link to="/" className='hover:bg-'>Home</Link>
+            <Link to="/gallery">Galeria</Link>
           </nav>
+          <div className="flex gap-7">
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </div>
         </div>
       </header>
       <main>
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/login' element={<Login/>}/>
-            <Route path='/register' element={<Register/>}/>
-          </Routes>
-        </BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
       </main>
+    </BrowserRouter>
     </>
   )
 }
