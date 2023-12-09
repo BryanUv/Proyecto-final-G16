5f5ccd441b8406b4eb7966472f467460eaabcb3a
Bryan Urquizo <bryanurquiso@gmail.com>
2023-12-09 18:22:25 -0500

    login

--- app-restaurante/src/pages/Login.jsx
+++ app-restaurante/src/pages/Login.jsx
@@ -1,9 +1,19 @@
-
+import FormLogin from "../components/Login/FormLogin";
 
 const Login = () => {
   return (
-    <h1 className="text-center">Login</h1>
-  )
-}
+    <div className="flex w-full h-screen bg-slate-900">
+      <div className="w-full flex items-center justify-center lg:w-1/2">
+        <FormLogin/>
+      </div>
+      <div className="hidden relative lg:flex h-full w-1/2 items-center justify-center bg-gray-200">
+        <div className="w-full h-full relative flex">
+          <img src="https://images.hola.com/imagenes/cocina/recetas/20191014150987/platos-de-lunes-a-viernes/0-729-379/portada-m.jpg"/>
+        </div>
+        <div className="w-full h-1/2 absolute bottom-0 bg-white/10 backdrop-blur-sm"/>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
