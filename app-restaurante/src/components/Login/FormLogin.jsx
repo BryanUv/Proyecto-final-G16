77a10670a0a6f20804f7b5ffc6223ccbdf31eb45
Bryan Urquizo <bryanurquiso@gmail.com>
2023-12-18 23:00:37 -0500

    login finish

--- app-restaurante/src/components/Login/FormLogin.jsx
+++ app-restaurante/src/components/Login/FormLogin.jsx
@@ -21,45 +21,48 @@ const FormLogin = () => {
         <p className="font-medium text-lg text-gray-500 mt-4">
           ¡Bienvenido de nuevo! Por favor ingrese sus datos.
         </p>
-        <div className="mt-8">
-          <div>
-            <label htmlFor="email" className="text-lg font-medium">
-              Usuario
-            </label>
-            <input
-              type="text"
-              id="email"
-              className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent"
-              placeholder="Ingresa tu email"
-            />
+        <form>
+          <div className="mt-8">
+            <div>
+              <label htmlFor="email" className="text-lg font-medium">
+                Usuario
+              </label>
+              <input
+                type="text"
+                id="email"
+                className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent"
+                placeholder="Ingresa tu email"
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="text-lg font-medium">
+                Contraseña
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent"
+                placeholder="Ingresa tu contraseña"
+                required
+              />
+            </div>
+            <div className="mt-8 flex flex-col gap-y-4">
+              <button className="hover:bg-slate-700 py-3 rounded-xl bg-slate-500 text-white text-lg font-bold">
+                Iniciar sesión
+              </button>
+            </div>
+            <div className="mt-8 flex justify-center items-center">
+              <p className="font-medium text-base">No tiene una cuenta?</p>
+              <Link
+                to="/register"
+                className="text-slate-500 text-base font-medium ml-2"
+              >
+                Regístrate
+              </Link>
+            </div>
           </div>
-          <div>
-            <label htmlFor="password" className="text-lg font-medium">
-              Contraseña
-            </label>
-            <input
-              type="password"
-              id="password"
-              className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent"
-              placeholder="Ingresa tu contraseña"
-            />
-          </div>
-          <div className="mt-8 flex flex-col gap-y-4">
-            <button className="hover:bg-slate-700 py-3 rounded-xl bg-slate-500 text-white text-lg font-bold">
-              Iniciar sesión
-            </button>
-          </div>
-          <div className="mt-8 flex justify-center items-center">
-            <p className="font-medium text-base">No tiene una cuenta?</p>
-            {/* <button className="text-violet-500 text-base font-medium ml-2">Registrate</button> */}
-            <Link
-              to="/register"
-              className="text-slate-500 text-base font-medium ml-2"
-            >
-              Regístrate
-            </Link>
-          </div>
-        </div>
+        </form>
       </div>
     </section>
   );
