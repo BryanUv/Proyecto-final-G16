014286ce272d3786a995a0d49b9d6fc3868ba9c6
Bryan Urquizo <bryanurquiso@gmail.com>
2023-12-27 20:25:32 -0500

    update, reserva responsive

--- app-restaurante/src/components/reservas/ListaReserva.jsx
+++ app-restaurante/src/components/reservas/ListaReserva.jsx
@@ -2,10 +2,10 @@ const ListaReserva = ({ reserva,eliminar, editar }) => {
   
   if (reserva.length === 0) {
     return (
-      <section className="w-1/4 p-4 bg-white/20 backdrop-blur-sm">
+      <section className="lg:w-1/4 p-4 bg-white/20 backdrop-blur-sm">
         <h2 className="text-2xl text-center mb-4">Reservas</h2>
 
-        <div className="flex justify-center items-center gap-4 h-96 text-2xl">
+        <div className="flex justify-center items-center gap-4 h-60 lg:h-96 text-2xl">
           No tiene Reservas.
         </div>
       </section>
@@ -14,7 +14,7 @@ const ListaReserva = ({ reserva,eliminar, editar }) => {
   
   return (
     <>
-      <section className="w-1/4 p-4 bg-none rounded-lg ">
+      <section className="lg:w-1/4 p-4 bg-none rounded-lg ">
         <h2 className="text-2xl text-center mb-4 text-white font-bold">RESERVACIONES</h2>
 
         {reserva.map((reserva) => {
@@ -22,21 +22,21 @@ const ListaReserva = ({ reserva,eliminar, editar }) => {
             <div className="flex flex-col gap-3 mb-4" key={reserva.id}>
               <div className="border-2 p-3 rounded-lg bg-white/20 backdrop-blur-sm ">
                 <div>
-                  <h4 className="font-semibold text-lg text-white underline">Datos de Reserva</h4>
+                  <h4 className="font-semibold text-lg lg:text-white underline">Datos de Reserva</h4>
                   <div>
-                    <strong className="text-white">Nombre de Cliente: {reserva.cliName}</strong> 
+                    <strong className="lg:text-white">Nombre de Cliente: {reserva.cliName}</strong> 
                   </div>
                   <div>
-                    <strong className="text-white">Cantidad de personas: {reserva.cliNumber}</strong> 
+                    <strong className="lg:text-white">Cantidad de personas: {reserva.cliNumber}</strong> 
                   </div>
                 </div>
                 <div>
-                  <h4 className="font-semibold text-lg text-white underline">Fecha y Hora de Reserva</h4>
+                  <h4 className="font-semibold text-lg lg:text-white underline">Fecha y Hora de Reserva</h4>
                   <div>
-                    <strong className="text-white">Fecha: {reserva.reservaDate}</strong> 
+                    <strong className="lg:text-white">Fecha: {reserva.reservaDate}</strong> 
                   </div>
                   <div>
-                    <strong className="text-white">Hora: {reserva.reservaTime}</strong> 
+                    <strong className="lg:text-white">Hora: {reserva.reservaTime}</strong> 
                   </div>
                 </div>
                 <div className="flex flex-col gap-3 mt-3">
