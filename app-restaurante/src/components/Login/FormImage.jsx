222142de14c13afc0aadb6840daf59e75b9f9ded
Bryan Urquizo <bryanurquiso@gmail.com>
2023-12-28 17:36:23 -0500

    update, image login

--- app-restaurante/src/components/Login/FormImage.jsx
+++ app-restaurante/src/components/Login/FormImage.jsx
@@ -3,7 +3,7 @@ const FormImage = () => {
   return (
     <>
       <div className="w-full h-full relative flex">
-        <img src="https://images.hola.com/imagenes/cocina/recetas/20191014150987/platos-de-lunes-a-viernes/0-729-379/portada-m.jpg"/>
+        <img src="https://media.istockphoto.com/id/1469161105/es/foto/bbq-asado-wagyu-new-york-carne-de-res-filete-o-filete-de-lomo-a-la-parrilla-en-un-plato-con.jpg?s=2048x2048&w=is&k=20&c=4lA3j6ChCbssape43Lut4gVU75VMkjaBY6Snlyiw_iw="/>
       </div>
       <div className="w-full h-1/2 absolute bottom-0 bg-white/10 backdrop-blur-sm"/>
     </>
